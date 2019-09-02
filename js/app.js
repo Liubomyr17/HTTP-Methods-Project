947bfbe09aadba3882214c28b1c55a561f83fab5
Izdepskyi <Ragnarlobrok91>
2019-09-02 23:25:02 +0300

    HTTP-Methods-Project

--- js/app.js
+++ js/app.js
@@ -13,16 +13,16 @@ const url = "http(s)://5cab83bbc85e05001452e9c4.mockapi.io/items";
 httpForm.addEventListener('submit', submitItem);
 //submit Items
 
-function submitItem(event){
+function submitItem(event) {
 
     event.preventDefault();
     const itemValue = itemInput.value;
     const imageValue = imageInput.value;
 
-    if(itemValue.length === 0 || imageValue.length === 0){
+    if (itemValue.length === 0 || imageValue.length === 0) {
         showFeedback('please enter valid values');
     }
-    else{
+    else {
         postItemAPI(imageValue, itemValue);
         imageInput.value = '';
         itemInput.value = '';
@@ -30,50 +30,50 @@ function submitItem(event){
 }
 
 //load items
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     //
-   getItemsAPI(showItems);
+    getItemsAPI(showItems);
 });
 
 //show Feedback
-function showFeedback(text){
+function showFeedback(text) {
     feedback.classList.add('showItem');
     feedback.innerHTML = `<p>${text}</p>`;
 
-    setTimeout(()=>{
+    setTimeout(() => {
         feedback.classList.remove('showItem');
     }, 3000);
 }
 
 //get items
-function getItemsAPI(cb){
+function getItemsAPI(cb) {
     const url = "https://5cab83bbc85e05001452e9c4.mockapi.io/items";
     const ajax = new XMLHttpRequest();
 
     ajax.open('GET', url, true);
 
-    ajax.onload = function(){
-        if(this.status === 200){
+    ajax.onload = function () {
+        if (this.status === 200) {
             cb(this.responseText);
-        }else{
+        } else {
             console.log("something went wrong");
         }
     };
 
-    ajax.onerror = function(){
+    ajax.onerror = function () {
         console.log('there was an error');
     };
 
     ajax.send();
 }
 //show items
-function showItems(data){
+function showItems(data) {
     const items = JSON.parse(data);
     //console.log(items);
     let info = '';
 
-    items.forEach(item =>{
-        info+=`
+    items.forEach(item => {
+        info += `
         <li class="list-group-item d-flex align-items-center 
         justify-content-between flex-wrap item my-2">
         <img src="${item.avatar}" id='itemImage' class='itemImage 
@@ -90,7 +90,7 @@ function showItems(data){
         </div>
        </li>`;
     });
-    
+
     itemList.innerHTML = info;
     //get Icons
     getIcons();
@@ -98,79 +98,79 @@ function showItems(data){
 
 //post items
 
-function postItemAPI(img,itemName){
-const avatar = `img/${img}.jpeg`;
-const name = itemName;
+function postItemAPI(img, itemName) {
+    const avatar = `img/${img}.jpeg`;
+    const name = itemName;
 
-const url = "https://5cab83bbc85e05001452e9c4.mockapi.io/items";
-const ajax = new XMLHttpRequest();
+    const url = "https://5cab83bbc85e05001452e9c4.mockapi.io/items";
+    const ajax = new XMLHttpRequest();
 
     ajax.open("POST", url, true);
 
     ajax.setRequestHeader('Content-Type',
-    'application/x-www-form-urlencoded');
+        'application/x-www-form-urlencoded');
 
-    ajax.onload = function(){
-         //   console.log(this.responseText);  
-         getItemsAPI(showItems);            
+    ajax.onload = function () {
+        //   console.log(this.responseText);  
+        getItemsAPI(showItems);
     };
 
-    ajax.onerror = function(){
+    ajax.onerror = function () {
         console.log('there was an error');
     };
 
     ajax.send(`avatar=${avatar}&name=${name}`);
 }
 //get icons
-function getIcons(){
+function getIcons() {
     const editIcon = document.querySelectorAll(".edit-icon");
     const deleteIcon = document.querySelectorAll(".delete-icon");
 
-    deleteIcon.forEach(icon =>{
+    deleteIcon.forEach(icon => {
         const itemID = icon.dataset.id;
-        icon.addEventListener('click', function(event){
+        icon.addEventListener('click', function (event) {
             event.preventDefault();
             //console.log(itemID);
             deleteItemAPI(itemID)
         });
-    });    
-editIcon.forEach(icon =>{
-const itemID = icon.dataset.id;
-icon.addEventListener('click',function(event){
-    event.preventDefault();
-    const parent = 
-    event.target.parentElement.parentElement.parentElement;
-    const img = parent.querySelector('.itemImage').src;
-    const name = parent.querySelector('.itemName').textContent;
-    //console.log(parent, img, name, itemID);
-    editItemUI(parent,img,name,itemID);
-});
-});
+    });
+    editIcon.forEach(icon => {
+        const itemID = icon.dataset.id;
+        icon.addEventListener('click', function (event) {
+            event.preventDefault();
+            const parent =
+                event.target.parentElement.parentElement.parentElement;
+            const img = parent.querySelector('.itemImage').src;
+            const name = parent.querySelector('.itemName').textContent;
+            //console.log(parent, img, name, itemID);
+            editItemUI(parent, img, name, itemID);
+        });
+    });
 }
 // delete user
-function deleteItemAPI(id){
+function deleteItemAPI(id) {
     const url = `https://5cab83bbc85e05001452e9c4.mockapi.io/items/${id}`;
     const ajax = new XMLHttpRequest();
 
     ajax.open('DELETE', url, true);
 
-    ajax.onload = function(){
-        if(this.status === 200){
+    ajax.onload = function () {
+        if (this.status === 200) {
             //console.log(this.responseText);
             getItemsAPI(showItems);
-        }else{
+        } else {
             console.log("something went wrong");
         }
     };
 
-    ajax.onerror = function(){
+    ajax.onerror = function () {
         console.log('there was an error');
     };
 
     ajax.send();
 }
 // edit user
-function editItemUI(parent,itemImg,name,itemID){
+function editItemUI(parent, itemImg, name, itemID) {
     event.preventDefault();
 
     itemList.removeChild(parent);
@@ -179,7 +179,7 @@ function editItemUI(parent,itemImg,name,itemID){
     const jpegIndex = itemImg.indexOf('.jpeg');
     console.log(imgIndex, jpegIndex);
 
-    const img = itemImg.slice(imgIndex + 4,jpegIndex);
+    const img = itemImg.slice(imgIndex + 4, jpegIndex);
     //console.log(img);
     itemInput.value = name.trim();
     imageInput.value = img;
@@ -191,41 +191,41 @@ function editItemUI(parent,itemImg,name,itemID){
 
 //function editItemAPI
 
-function editItemAPI(){
+function editItemAPI() {
     event.preventDefault();
     const id = editedItemID;
 
     const itemValue = itemInput.value;
     const imageValue = imageInput.value;
 
-    if(itemValue.length === 0 || imageValue.length === 0){
+    if (itemValue.length === 0 || imageValue.length === 0) {
         showFeedback("please enter valid values");
-    }else{
-       const img = `img/${imageValue}.jpeg`;
-       const name = itemValue;
+    } else {
+        const img = `img/${imageValue}.jpeg`;
+        const name = itemValue;
 
-    const url = `https://5cab83bbc85e05001452e9c4.mockapi.io/items/${id}`;
+        const url = `https://5cab83bbc85e05001452e9c4.mockapi.io/items/${id}`;
 
-    const ajax = new XMLHttpRequest();
+        const ajax = new XMLHttpRequest();
 
-    ajax.open("PUT", url, true);
+        ajax.open("PUT", url, true);
 
-    ajax.setRequestHeader('Content-Type',
-    'application/x-www-form-urlencoded');
+        ajax.setRequestHeader('Content-Type',
+            'application/x-www-form-urlencoded');
 
-    ajax.onload = function(){
-         console.log(this.responseText);  
-         reverseForm()            
-    };
+        ajax.onload = function () {
+            console.log(this.responseText);
+            reverseForm()
+        };
 
-    ajax.onerror = function(){
-        console.log('there was an error');
-    };
+        ajax.onerror = function () {
+            console.log('there was an error');
+        };
 
-    ajax.send(`avatar=${img}&name=${name}`);
-    }    
+        ajax.send(`avatar=${img}&name=${name}`);
+    }
 }
-function reverseForm(){
+function reverseForm() {
     itemInput.value = "";
     imageInput.value = "";
     submtiBtn.innerHTML = 'Add Item';
